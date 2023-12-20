eae894cc97fec126500cc2413cdebc506489668f
giacomoDuePiuDue <glr.lorussogiacomo@gmail.com>
2023-12-20 06:10:44 +0100

    manca il delete e il patch dipendente

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,22 +1,45 @@
 import { Component,inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { EmployeeServiceService } from '../service/employee-service.service';
 import { EmployeeInterface } from '../interface/employee-interface';
+import { RefNationalitaInterface } from '../interface/ref-nazionalita-interface';
 
 @Component({
   selector: 'app-details',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './details.component.html',
   styleUrl: './details.component.css'
 })
 export class DetailsComponent {
+  modificaAreaPersonale:boolean;
   route:ActivatedRoute=inject(ActivatedRoute);
   employeeService=inject(EmployeeServiceService)
   employee:EmployeeInterface|undefined;
+  nazionalitaList:RefNationalitaInterface[]|undefined;
   id=Number(this.route.snapshot.params['id']);
 
   constructor()  {
     this.employeeService.getEmployeeById(this.id).then(x=>{this.employee=x;});
+    this.employeeService.getAllRefNaionalita().then(x=>{this.nazionalitaList=x});
+    this.modificaAreaPersonale=false;
   }
+
+  ModificaDipendete(evento:Event)  {
+    //this.employeeService.patchEmployeeById((<HTMLInputElement>evento.target).value).then();
+  }
+
+  ModificaArea():void  {
+    if(this.modificaAreaPersonale==false)  {
+      this.modificaAreaPersonale=true;
+      return;
+    }
+    this.modificaAreaPersonale=false;    
+  }
+
+  CancellaDipendete() {
+    this.employeeService.deleteEmployeeById(this.id).then();
+  }
+
 }
--- src/app/service/employee-service.service.ts
+++ src/app/service/employee-service.service.ts
@@ -1,15 +1,34 @@
 import { Injectable } from '@angular/core';
 import { EmployeeInterface } from '../interface/employee-interface';
+import { RefNationalitaInterface } from '../interface/ref-nazionalita-interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeServiceService {
-  url:string="http://localhost:8080/dipendente";
+  url:string="http://localhost:8080/";
   costructor(){}
 
   async getEmployeeById(id:Number):Promise<EmployeeInterface|undefined>{
-    let url=this.url+`/getByID?id=${id}`;
+    let url=this.url+`dipendente/getByID?id=${id}`;
+    const response = await fetch(url);
+    const data = await response.json() ?? [];
+    return data
+  }
+  async getAllRefNaionalita():Promise<RefNationalitaInterface[]>{
+    let url=this.url+`refnazionalita/listanazionalita`;
+    const response = await fetch(url);
+    const data = await response.json() ?? [];
+    return data
+  }
+  async patchEmployeeById(dipendente:EmployeeInterface):Promise<void>{
+    let url=this.url+`dipendente//modificaDipendente`;
+    const response = await fetch(url);
+    const data = await response.json() ?? [];
+    return data
+  }
+  async deleteEmployeeById(id:Number):Promise<void>{
+    let url=this.url+`dipendente/esercizio_4?id=${id}`;
     const response = await fetch(url);
     const data = await response.json() ?? [];
     return data
